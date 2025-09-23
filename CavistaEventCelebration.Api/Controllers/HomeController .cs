66b6c29e3344fde0151b05e07505a09bcb2d2fb1
mulki1 <baniaz4mulki@gmail.com>
2025-09-23 17:27:14 +0100

    get logged in user info

--- CavistaEventCelebration.Api/Controllers/HomeController .cs
+++ CavistaEventCelebration.Api/Controllers/HomeController .cs
@@ -1,10 +1,13 @@
-﻿using CavistaEventCelebration.Api.Services.Interface;
+﻿using System.Security.Claims;
+using CavistaEventCelebration.Api.Services.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CavistaEventCelebration.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class HomeController : ControllerBase
     {
         private readonly IDashBoardService _service;
@@ -17,6 +20,10 @@ namespace CavistaEventCelebration.Api.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            var result = await _service.Get();
+            result.UserId = User?.FindFirstValue("id") != null && !string.IsNullOrEmpty(User?.FindFirstValue("id")) ? Guid.Parse(User?.FindFirstValue("id")) : Guid.Empty;
+            result.UserEmail = User?.FindFirstValue(ClaimTypes.Email) ?? string.Empty;
+            result.UserName = User?.FindFirstValue(ClaimTypes.Name) ?? string.Empty;
             return Ok(await _service.Get());
         }
     }
